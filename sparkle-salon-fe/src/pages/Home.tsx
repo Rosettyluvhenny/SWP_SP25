880b8fb1991ca097c4509179022c3525e5a68343
KietNguyen279 <nkiet1319@gmail.com>
2025-02-09 10:43:45 +0700

    modified Home.tsx

--- sparkle-salon-fe/src/pages/Home.tsx
+++ sparkle-salon-fe/src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Link} from "react-router-dom";
+
 export default function Home() {
     return (
         <div className="pt-16 flex flex-col"> 
@@ -7,8 +9,8 @@ export default function Home() {
                     <h1 className="text-6xl font-bold leading-tight">Sparkle Salon</h1>
                     <p className="mt-2 text-lg text-gray-400">Salon hàng đầu về trị liệu da</p>
                     <p className="mt-2 text-lg text-gray-400">Đặt lịch và tới trực tiếp để chúng tôi có thể phục vụ bạn chu đáo hơn</p>
-                    <button className="mt-6 bg-[#f8a89c] hover:bg-[#f6a18f] text-black px-6 py-3 rounded-lg font-semibold text-lg">
-                        Đặt lịch ngay →
+                    <button className="mt-6 bg-[#f8a89c] hover:bg-[#ee8874] text-black px-6 py-3 rounded-lg font-semibold text-lg">
+                        <Link to="/contact">Đặt lịch ngay →</Link>
                     </button>
                 </div>
             </div>
