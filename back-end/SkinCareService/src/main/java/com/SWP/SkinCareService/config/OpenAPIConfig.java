717e6ae322915e0fcc0da4ac318a4bcb2810d45a
RosettyVn <stevexamcoi@gmail.com>
2025-02-18 16:05:08 +0700

    swagger integration

---
+++ back-end/SkinCareService/src/main/java/com/SWP/SkinCareService/config/OpenAPIConfig.java
@@ -0,0 +1,14 @@
+package com.SWP.SkinCareService.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
+}
