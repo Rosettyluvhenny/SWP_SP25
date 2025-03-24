fab9e45558e52a8d985784c24bdc48d7a7ce8aa6
RosettyVn <stevexamcoi@gmail.com>
2025-03-24 21:38:26 +0700

    side bar staff commit

---
+++ sparkle-salon-fe/src/pages/Staff.tsx
@@ -0,0 +1,10 @@
+import StaffSideBar from "../components/StaffSideBar";
+
+export default function Staff(){
+    return (
+        <div className="flex h-screen bg-white">
+        <StaffSideBar/>
+        
+        </div>
+    )
+}
